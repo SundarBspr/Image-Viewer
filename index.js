372eabff0b736553878ab451f5fb3e50c673e1ef
SundarBspr <bhagavatula.balarka@sprinklr.com>
2022-05-21 00:37:31 +0530

    Third commit(events for mouse are working fine)

--- index.js
+++ index.js
@@ -6,47 +6,59 @@ function ellipsify (str) {
   return str;
 }
 }
-
 function jsonTask(items){
   const InitialRightImage=document.querySelector(".rImg")
   InitialRightImage.setAttribute("src",items[0].previewImage)
   InitialRightImage.setAttribute("width","500px")
   InitialRightImage.setAttribute("height","700px")
     items.forEach(item => {
-      // console.log(item.title)
       const left=document.querySelector(".left-item")
-      const right=document.querySelector(".right-item")
       const Box=document.createElement("div")
       Box.style.display="grid";
-      Box.style.gridTemplateColumns="80px 250px"
-      // Box.style.gridTemplateRows="70px"
-      Box.insertAdjacentHTML('afterBegin',`
-        <img src=${item["previewImage"]} width="40px" height="40px" margin:"0px">
+      Box.style.gridTemplateColumns="60px 240px"
+      Box.insertAdjacentHTML('beforeend',`
+        <img src=${item["previewImage"]} class="logo" width="40px" height="40px" margin:"0px">
         <div class="titleImage" margin:"0px">${item["title"]}</div>
       `)
-      
       const rightImg=document.createElement("img")
       rightImg.setAttribute("src",item["previewImage"])
       
       // console.log(logo)
       
-      Box.style.margin="3px"
+      Box.style.margin="3px 0px"
+      Box.classList.add("box")
 
       left.append(Box)
-      rightImg.setAttribute("width","500px")
-      rightImg.setAttribute("height","700px")
-      // right.append(rightImg)//put event listener and change src of .rImg
       
     });
     const titleImage=document.querySelectorAll(".titleImage")
     titleImage.forEach((item)=>{
-      console.log(item.innerHTML)
+      // console.log(item.innerHTML)
       item.innerHTML=ellipsify(item.innerHTML)
       item.style.marginTop+="10px"
       item.style.marginLeft="0px"
+    })   
+    const logos=document.querySelectorAll(".logo")
+    console.log(logos)
+    logos.forEach((logo)=>{
+      logo.addEventListener("click",(e)=>{
+        InitialRightImage.setAttribute("src",logo.currentSrc)
+      })
     })
-    
+    const boxes=document.querySelectorAll(".box")
+    console.log(boxes)
+    boxes.forEach((box)=>{
+      box.addEventListener("mouseover",(e)=>{
+        box.style.backgroundColor="skyblue"
+      })
+    })
+    boxes.forEach((box)=>{
+      box.addEventListener("mouseleave",(e)=>{
+        box.style.backgroundColor="white"
+      })
+    })
 }
+
 
 
 fetch('./Untitled.txt')
@@ -55,3 +67,4 @@ fetch('./Untitled.txt')
       // console.log(jsonResponse)
       jsonTask(jsonResponse)
     }) 
+
